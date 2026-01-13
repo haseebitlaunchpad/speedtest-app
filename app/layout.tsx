37aaf51f2836fcf40d3cf969ec19b52330c2c7ad
demo <demo@demo.com>
2026-01-13 17:12:12 +0500

    Opportunity form name changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7722035183156465" crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
